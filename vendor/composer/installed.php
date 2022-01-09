222ac2070519377e92762948ef55cd87efaba53e
ec136 <346325170@qq.com>
2022-01-09 11:20:36 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v2.0.6',
+    'version' => '2.0.6.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'haihua/swoftamqp',
+  ),
+  'versions' => 
+  array (
+    'haihua/swoftamqp' => 
+    array (
+      'pretty_version' => 'v2.0.6',
+      'version' => '2.0.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'swoft/stdlib' => 
+    array (
+      'pretty_version' => 'v2.0.11',
+      'version' => '2.0.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9609105a54388515c84b408e4abb5171db25292b',
+    ),
+  ),
+);
